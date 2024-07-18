cd6fef89a53e40165e460d3812b56715012974ed
Esosek <67476790+Esosek@users.noreply.github.com>
2024-07-18 14:57:39 +0200

    Added CartContext

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Cart from "@/components/cart/Cart";
 import ProductList from "@/components/products/ProductList";
+import { CartContextProvider } from "@/context/CartContext";
 import getMeals from "@/lib/products";
 
 export default function Home() {
   const meals = getMeals();
   return (
     <main className="flex flex-col items-start gap-4 px-4 py-2 md:py-16 md:px-20 md:flex-row">
-      <ProductList products={meals} />
-      <Cart />
+      <CartContextProvider>
+        <ProductList products={meals} />
+        <Cart />
+      </CartContextProvider>
     </main>
   );
 }
--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -1,14 +1,87 @@
-import { createContext, type PropsWithChildren } from "react";
+"use client";
+import { createContext, useState, type PropsWithChildren } from "react";
+import { Product } from "@/types/product";
 
-type CartContextType = {};
+type CartContextType = {
+  items: { product: Product; quantity: number }[];
+  addItem: (product: Product) => void;
+  removeItem: (productId: number, quantity: number) => void;
+};
 
-const defaultValue = {};
+const defaultValue = {
+  items: [],
+  addItem: () => {},
+  removeItem: () => {},
+};
 
 const CartContext = createContext<CartContextType>(defaultValue);
 
 export function CartContextProvider({ children }: PropsWithChildren) {
+  const [items, setItems] = useState([
+    {
+      product: {
+        id: 0,
+        image: {
+          thumbnail: "/assets/images/image-waffle-thumbnail.jpg",
+          mobile: "/assets/images/image-waffle-mobile.jpg",
+          tablet: "/assets/images/image-waffle-tablet.jpg",
+          desktop: "/assets/images/image-waffle-desktop.jpg",
+        },
+        name: "Waffle with Berries",
+        category: "Waffle",
+        price: 6.5,
+      },
+      quantity: 3,
+    },
+  ]);
+
+  function addItem(product: Product) {
+    setItems((prevItems) => {
+      const itemIndex = prevItems.findIndex(
+        (item) => item.product.id === product.id
+      );
+
+      // Increase quantity
+      if (itemIndex > -1) {
+        const updatedItems = [...prevItems];
+        updatedItems[itemIndex] = {
+          ...updatedItems[itemIndex],
+          quantity: updatedItems[itemIndex].quantity + 1,
+        };
+        return updatedItems;
+      }
+
+      // Add new item
+      return [...prevItems, { product: product, quantity: 1 }];
+    });
+  }
+
+  function removeItem(id: number, quantity = 1) {
+    setItems((prevItems) => {
+      const itemIndex = prevItems.findIndex((item) => item.product.id === id);
+
+      if (itemIndex < -1) {
+        return prevItems;
+      }
+
+      // Decrement quantity
+      else if (prevItems[itemIndex].quantity > quantity) {
+        const updatedItems = [...prevItems];
+        updatedItems[itemIndex] = {
+          ...updatedItems[itemIndex],
+          quantity: updatedItems[itemIndex].quantity - quantity,
+        };
+        return updatedItems;
+      }
+      // Remove the item completely
+      return prevItems.filter((item) => item.product.id !== id);
+    });
+  }
+
   return (
-    <CartContext.Provider value={defaultValue}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ items, addItem, removeItem }}>
+      {children}
+    </CartContext.Provider>
   );
 }
 
