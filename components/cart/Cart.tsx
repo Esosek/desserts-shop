43e22dfba8603c6ae2040e2f38e54ea4848394c4
Esosek <67476790+Esosek@users.noreply.github.com>
2024-07-18 15:17:22 +0200

    Added cartItemQuantity to Cart heading

--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -1,10 +1,18 @@
+"use client";
+import { useContext } from "react";
+
 import EmptyCart from "./EmptyCart";
+import CartContext from "@/context/CartContext";
 
 export default function Cart() {
+  const cartItemQuantity = useContext(CartContext).items.reduce(
+    (acc, item) => acc + item.quantity,
+    0
+  );
   return (
     <div className=" bg-white my-4 px-6 pb-10 min-w-80 rounded-md md:my-0">
       <h2 className="justify-self-start text-red text-lg font-bold my-6">
-        Your Cart (0)
+        Your Cart ({cartItemQuantity})
       </h2>
       <EmptyCart />
     </div>
--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -33,6 +33,21 @@ export function CartContextProvider({ children }: PropsWithChildren) {
       },
       quantity: 3,
     },
+    {
+      product: {
+        id: 1,
+        image: {
+          thumbnail: "/assets/images/image-creme-brulee-thumbnail.jpg",
+          mobile: "/assets/images/image-creme-brulee-mobile.jpg",
+          tablet: "/assets/images/image-creme-brulee-tablet.jpg",
+          desktop: "/assets/images/image-creme-brulee-desktop.jpg",
+        },
+        name: "Vanilla Bean Crème Brûlée",
+        category: "Crème Brûlée",
+        price: 7.0,
+      },
+      quantity: 1,
+    },
   ]);
 
   function addItem(product: Product) {
