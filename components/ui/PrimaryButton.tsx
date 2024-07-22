65d3682ecffeba3646606b0edf973f3291935115
Ales Zima <ales@cpex.cz>
2024-07-22 07:44:08 +0200

    Added PrimaryButton component

---
+++ components/ui/PrimaryButton.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+
+type PrimaryButtonProps = {
+  children: ReactNode;
+  onClick: () => void;
+};
+
+export default function PrimaryButton({
+  children,
+  onClick,
+}: PrimaryButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-red-400 w-full text-rose-50 rounded-full p-3 my-6 shadow-md hover:bg-red-700 transition-colors duration-100"
+    >
+      {children}
+    </button>
+  );
+}
