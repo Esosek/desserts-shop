fe46bfd58a2623522ec328eb576e88cf2e12e1af
Ales Zima <ales@cpex.cz>
2024-07-22 16:50:34 +0200

    Added filtering by category logic

---
+++ components/products/PoductFilter.tsx
@@ -0,0 +1,42 @@
+import { Product } from "@/types/product";
+import { ChangeEvent } from "react";
+
+type ProductFilterProps = {
+  products: Product[];
+  onFilterChanged: (newFilter: string) => void;
+};
+
+export default function ProductFilter({
+  products,
+  onFilterChanged,
+}: ProductFilterProps) {
+  const categories = products.reduce((acc: string[], value) => {
+    if (!acc.includes(value.category)) {
+      acc.push(value.category);
+    }
+    return acc;
+  }, []);
+
+  function handleFilterChange(event: ChangeEvent<HTMLSelectElement>) {
+    onFilterChanged(event.currentTarget.value);
+  }
+  return (
+    <div className="flex items-center gap-4">
+      <label htmlFor="filter-select" className="text-sm">
+        Filter by category
+      </label>
+      <select
+        id="filter-select"
+        name="filter-select"
+        onChange={handleFilterChange}
+      >
+        <option value="none">None</option>
+        {categories.map((cat) => (
+          <option key={cat.toLowerCase()} value={cat.toLowerCase()}>
+            {cat}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
--- components/products/ProductList.tsx
+++ components/products/ProductList.tsx
@@ -1,16 +1,36 @@
+"use client";
+
 import { Product } from "@/types/product";
 import ProductItem from "./ProductItem";
+import ProductFilter from "./PoductFilter";
+import { useState } from "react";
 
 type ProductListProps = {
   products: Product[];
 };
 
 export default function ProductList({ products }: ProductListProps) {
+  const [categoryFilter, setCategoryFilter] = useState<string>("none");
+
+  const filteredProducts =
+    categoryFilter === "none"
+      ? products
+      : products.filter(
+          (product) => product.category.toLocaleLowerCase() === categoryFilter
+        );
+
+  function onFilterChanged(newFilter: string) {
+    setCategoryFilter(newFilter);
+  }
   return (
     <div className="w-full">
-      <h1 className="font-bold text-3xl my-6">Desserts</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="font-bold text-3xl my-6">Desserts</h1>
+        <ProductFilter products={products} onFilterChanged={onFilterChanged} />
+      </div>
+
       <ul className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-x-5 gap-y-7">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <ProductItem key={product.name} product={product} />
         ))}
       </ul>
