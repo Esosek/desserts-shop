4354225ace84f37ae0008223678d220113e47dce
Ales Zima <ales@cpex.cz>
2024-07-22 07:25:23 +0200

    Added margin to EmptyCart

--- components/cart/EmptyCart.tsx
+++ components/cart/EmptyCart.tsx
@@ -4,7 +4,7 @@ import cakeImage from "@/public/assets/images/illustration-empty-cart.svg";
 
 export default function EmptyCart() {
   return (
-    <div className="grid gap-4 justify-items-center">
+    <div className="grid gap-4 justify-items-center mb-10">
       <Image src={cakeImage} alt="Image of cake" />
       <p className="text-rose-400 font-semibold text-sm">
         Your added items will appear here
