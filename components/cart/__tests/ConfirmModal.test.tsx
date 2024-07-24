79dab4933d9a095ec33906c5b7410f0de4a1f1ce
Esosek <67476790+Esosek@users.noreply.github.com>
2024-07-24 07:12:28 +0200

    Added ConfirmModal tests shell

---
+++ components/cart/__tests/ConfirmModal.test.tsx
@@ -0,0 +1,11 @@
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ConfirmModal from "../ConfirmModal";
+
+describe("ConfirmModal", () => {
+  test("renders icon, confirmation text, product items and start new order button", () => {});
+
+  test("renders correct total price", () => {});
+
+  test("closes when start new button or backdrop is clicked", () => {});
+});
